213910ee430c86d4baa94a4efd5f3cbc17279ba9
Michael Fouche <mfouche@impact.com>
2025-07-26 12:59:12 +0200

    improve work breakdown expert

--- src/main/java/hackathon/hackathon2025/agents/WorkBreakdownExpert.java
+++ src/main/java/hackathon/hackathon2025/agents/WorkBreakdownExpert.java
@@ -6,6 +6,6 @@ import dev.langchain4j.service.UserMessage;
 
 public interface WorkBreakdownExpert {
     @UserMessage("You are a work estimation expert. {{it}}")
-    @SystemMessage("You are an expert in breaking down a work statement into what is required to complete the software engineering problem for an agile software engineering team. Each work item should be listed on a new line.")
+    @SystemMessage("You are an expert in breaking down a work statement into what is required to complete the software engineering problem for an agile software engineering team. Be very specific on tasks. dependency on tasks should be specified and grouped together. Each work item should be listed on a new line.")
     String chat(String userMessage);
 }
